292d45f0e2f970c374c2ef0d8898e461e7803b40
OBIDIKE-BON <bonifaceobidike@gmail.com>
2023-08-11 10:41:36 +0100

    style

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 if __name__ == '__main__':
     import hidden_4
-    print ([v for v in dir() if not v.startswith('__')])
+    print([v for v in dir() if not v.startswith('__')])
