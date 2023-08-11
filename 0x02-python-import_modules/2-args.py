4486ff55d47353bee8bd2e58d7d3bdfe9b5bdb4a
OBIDIKE-BON <bonifaceobidike@gmail.com>
2023-08-11 09:19:46 +0100

    accounted for 0 args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,7 +2,13 @@
 if __name__ == '__main__':
     import sys
     ac = len(sys.argv) - 1
-    print("{} {}:".format(ac, ("argument" if ac == 1 else "arguments")))
+    if ac == 0:
+        sufix = "arguments." 
+    elif ac == 1:
+        sufix = "argument:" 
+    else:
+      sufix = "arguments:"
+    print("{} {}".format(ac, sufix))
     for index, arg in enumerate(sys.argv, start=0):
         if index != 0:
             print("{}: {}".format(index, arg))
