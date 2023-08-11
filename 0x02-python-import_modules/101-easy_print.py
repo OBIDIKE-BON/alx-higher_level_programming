226dcd41d8968ab89502e5ae6e90f7788bf8cf9a
OBIDIKE-BON <bonifaceobidike@gmail.com>
2023-08-11 11:54:06 +0100

    new way

--- 0x02-python-import_modules/101-easy_print.py
+++ 0x02-python-import_modules/101-easy_print.py
@@ -1,2 +1,3 @@
 #!/usr/bin/python3
-__import__("os").write(1, "#pythoniscool\n".encode("UTF-8"))
+# __import__("os").write(1, "#pythoniscool\n".encode("UTF-8"))
+exec(chr(112)+chr(114)+chr(105)+chr(110)+chr(116)+chr(40)+chr(34)+"#pythoniscool"+chr(34)+chr(41))
