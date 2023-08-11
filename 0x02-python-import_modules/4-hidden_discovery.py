cdf4330df5b343dc4cb515949da2c3da0caaac6f
OBIDIKE-BON <bonifaceobidike@gmail.com>
2023-08-11 10:49:35 +0100

    new

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 if __name__ == '__main__':
     import hidden_4
-    print(v for v in dir(hidden_4) if not v.startswith('__'))
+    for memb in dir(hidden_4):
+        if not memb.startswith('__'):
+            print(memb)
