be26766ef00a1015d2532edac63a8a3c1a4e2e8f
OBIDIKE-BON <bonifaceobidike@gmail.com>
2023-08-13 20:11:42 +0100

    style

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,7 +3,7 @@ def no_c(my_string):
     le = len(my_list)
     if le > 0:
         new_str = ""
-        for i in range(len -1):
+        for i in range(len - 1):
             if (my_string[i] != "c") and my_string[i] != "C":
                 new_str += my_string[i]
         return new_str
