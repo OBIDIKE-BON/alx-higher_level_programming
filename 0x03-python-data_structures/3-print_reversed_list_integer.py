87a4b0a9c793feb5f3c5d5717b5e3fe51c92566b
OBIDIKE-BON <bonifaceobidike@gmail.com>
2023-08-13 19:03:57 +0100

    print list un reverse order

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    le = len(my_list)
-    if ((idx >= 0) and (le > 0)) and ((idx <= le - 1)):
+    if len(my_list) > 0:
         for i in range(len(my_list) - 1, -1, -1):
             print(my_list[i])
