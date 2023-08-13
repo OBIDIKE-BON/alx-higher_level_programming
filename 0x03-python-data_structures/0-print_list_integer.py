b3350536db29020c075fce3ed4af9aa6b8c51394
OBIDIKE-BON <BONIFACEOBIDIKE@GMAIL.COM>
2023-08-13 08:15:58 -0700

    print a list of integers

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    if len(my_list) > 0:
-        for i in my_list:
-            print("{:d}".format(i)
+    if __name__ == "main":
+        if len(my_list) > 0:
+            for i in my_list:
+                print("{:d}".format(i))
