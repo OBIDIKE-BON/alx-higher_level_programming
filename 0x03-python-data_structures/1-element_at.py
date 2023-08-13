ec39cf8ab8010b3d596a0dfa831e6cb301d275be
OBIDIKE-BON <BONIFACEOBIDIKE@GMAIL.COM>
2023-08-13 09:24:36 -0700

    print item from a list

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,4 +2,4 @@
 def element_at(my_list, idx):
     le = len(my_list)
     if idx >= 0 and le > 0 and le - 1 <= idx:
-        print("{:d}".format(my_list[idx]))
+        print("Element at index {:d} is {:d}".format(idx, my_list[idx]))
