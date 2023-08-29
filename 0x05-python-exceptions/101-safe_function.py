9d9eacba0fd5158f74ea0327447a8c1a0f375baf
OBIDIKE-BON <bonifaceobidike@gmail.com>
2023-08-29 05:55:48 +0100

    style

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -5,10 +5,9 @@ import sys
 
 def safe_function(fct, *args):
     try:
-        resource = fct(*args)
+        res = fct(*args)
     except Exception as e:
         print("Exception: {}".format(e), file=sys.stderr)
         return None
     else:
-        return resource
-
+        return res
