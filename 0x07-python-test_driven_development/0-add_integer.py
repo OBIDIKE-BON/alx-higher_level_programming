492432f57b2c5ff5d6ccbf0fbec08eb815161447
OBIDIKE-BON <bonifaceobidike@gmail.com>
2023-09-06 04:06:09 +0100

    first test

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""Contains a function to add two integers"""
+
+
+def add_integer(a, b=98):
+    """Adds two integers
+    Args:
+        a (int or double): first number.
+        b (int or double): second number.
+    Returns:
+        sum of a and b.
+    """
+    if type(a) != int and type(a) != float:
+        raise TypeError("a must be an integer")
+    if type(b) != int and type(b) != float:
+        raise TypeError("b must be an integer")
+    if type(a) is float:
+        a = int(a)
+    if type(b) is float:
+        b = int(b)
+    return (a + b)
+
+
+print(add_integer(1, 2))
+print(add_integer(100, -2))
+print(add_integer(2))
+print(add_integer(100.3, -2))
+try:
+    print(add_integer(4, "School"))
+except Exception as e:
+    print(e)
+try:
+    print(add_integer(None))
+except Exception as e:
+    print(e)
