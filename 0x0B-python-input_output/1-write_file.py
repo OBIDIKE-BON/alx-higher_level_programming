c5687608e7e9c6e9140d30416d06503d2e4c8492
OBIDIKE-BON <bonifaceobidike@gmail.com>
2023-09-12 18:55:23 +0100

    function that reads and print a file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""This Module creates a function to read and write"""
+
+
+def read_file(filename=""):
+    """function that reads and print a file"""
+    with open(filename, "r") as f:
+        print(f.read())
