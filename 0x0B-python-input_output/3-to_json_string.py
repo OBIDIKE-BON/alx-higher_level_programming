a7ba392086aadb1465c814161868aec6a43a0a8c
OBIDIKE-BON <bonifaceobidike@gmail.com>
2023-09-12 19:59:19 +0100

    ok

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -5,7 +5,4 @@ import json
 
 def to_json_string(my_obj):
     """a function that returns json from an object"""
-    try:
-        return json.dumps(my_obj)
-    except:
-        raise TypeError(f"{my_obj} is not JSON serializable")
+    return json.dumps(my_obj)
