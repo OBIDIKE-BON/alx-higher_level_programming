f4520bc9a3e18c89acb8983733b364fe60392759
OBIDIKE-BON <bonifaceobidike@gmail.com>
2023-09-12 19:56:57 +0100

    ok

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """This Module creates a function that returns json"""
+import json
 
 
 def to_json_string(my_obj):
     """a function that returns json from an object"""
-    import json
     try:
         return json.dumps(my_obj)
     except:
