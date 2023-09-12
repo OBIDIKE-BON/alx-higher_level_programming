14b24030232e383a184152db9e8a46fe8670c180
OBIDIKE-BON <bonifaceobidike@gmail.com>
2023-09-12 19:13:50 +0100

    name error

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -4,5 +4,5 @@
 
 def write_file(filename="", text=""):
     """a function to write to a file"""
-    with open(filename, "w") as file:
+    with open(filename, "w") as f:
         return f.write(text)
