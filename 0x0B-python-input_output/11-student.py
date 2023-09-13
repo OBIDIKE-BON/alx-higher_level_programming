ab2897445f216e044f5b87de098e73159bde9ccd
OBIDIKE-BON <bonifaceobidike@gmail.com>
2023-09-13 05:57:19 +0100

    a new way

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -16,15 +16,15 @@ class Student:
     def to_json(self, attrs=None):
         """Retrieves a dictionary representation of the instance of the class.
         """
-        if attrs is None:
-            return self.__dict__
-        else:
+        if attrs and (attrs is list) and len(attrs) > 0 and (attrs[0] is str):
             new_dict = dict()
             old_dict = self.__dict__
             for (key, value) in old_dict.items():
                 if key in attrs:
                     new_dict[key] = value
             return new_dict
+        else:
+            return self.__dict__
 
     def reload_from_json(self, json):
         """Uses the ``json`` dictionary to set the attributes of the instance.
