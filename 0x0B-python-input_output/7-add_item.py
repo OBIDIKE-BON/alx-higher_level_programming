82ea61bedb9a011603dc8347545979b483818706
OBIDIKE-BON <bonifaceobidike@gmail.com>
2023-09-13 04:15:11 +0100

    add all arguments to a Python list

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""add all arguments to a Python list, and then save them to a file
+"""
+import sys
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+filename = "add_item.json"
+try:
+    mList = load_from_json_file(filename)
+except:
+    mList =[]
+
+for i in range(len(sys.argv)):
+    mList.append(sys.argv[i])
+
+save_to_json_file(mList, filename)
