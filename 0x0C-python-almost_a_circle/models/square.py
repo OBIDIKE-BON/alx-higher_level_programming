e80d4ab5a069a03c5eeacbb34918b4e73822b22b
OBIDIKE-BON <bonifaceobidike@gmail.com>
2023-09-19 00:56:45 +0100

    Square class creation

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Module containing ``Square`` class definition."""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """``Square`` class definition"""
+    def __init__(self, size, x=0, y=0, id=None):
+        """Initialize instance of ``Square`` class."""
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """string representation of instance."""
+        return "[Square] ({}) {}/{} - {}".format(
+                self.id, self.x, self.y, self.width)
+
+    @property
+    def size(self):
+        """public ``size`` property"""
+        return self.width
+
+    @size.setter
+    def size(self, size):
+        self.width = size
+        self.height = size
