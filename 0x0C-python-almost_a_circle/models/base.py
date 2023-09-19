ad07ee80a0ef56d209e1abed02bc7335d7c38f43
OBIDIKE-BON <bonifaceobidike@gmail.com>
2023-09-19 02:25:51 +0100

    the json representation of a list

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """Module containing the ``Base`` class definition."""
+import json
 
 
 class Base:
@@ -13,3 +14,10 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        """returns the json representation of a list"""
+        if list_dictionaries is None:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
