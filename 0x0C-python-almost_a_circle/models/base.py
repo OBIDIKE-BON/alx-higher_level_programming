493f34ba4636ae28ddb4aad1e3a7ef9e012e5c01
OBIDIKE-BON <bonifaceobidike@gmail.com>
2023-09-19 02:31:08 +0100

    added case for an empty list

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -17,7 +17,7 @@ class Base:
 
     def to_json_string(list_dictionaries):
         """returns the json representation of a list"""
-        if list_dictionaries is None:
+        if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
         else:
             return json.dumps(list_dictionaries)
