5ea32e9a6ac155cc0521b2a37a12fc681bf6de1b
OBIDIKE-BON <bonifaceobidike@gmail.com>
2023-09-19 02:33:24 +0100

    staticmethod

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -15,8 +15,9 @@ class Base:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
+    @staticmethod
     def to_json_string(list_dictionaries):
-        """returns the json representation of a list"""
+        """returns the json representation of a list."""
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
         else:
