b31e04173bff514fc4225aff78d6d09d4f7254e4
OBIDIKE-BON <bonifaceobidike@gmail.com>
2023-11-13 20:02:42 +0100

    style

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -3,7 +3,7 @@ const args = process.argv;
 const size = args.length;
 if (size > 3) {
   console.log('Arguments found');
-} else if (size > 2){
+} else if (size > 2) {
   console.log('Argument found');
 } else {
   console.log('No argument');
