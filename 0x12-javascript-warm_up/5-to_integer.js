c97570f428e1363901807b63bd9fdfff1e4a3539
OBIDIKE-BON <bonifaceobidike@gmail.com>
2023-11-13 20:30:41 +0100

    My number:

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const args = process.argv;
 if (!isNaN(parseInt(args[2]))) {
-  console.log(parseInt(args[2]));
+  console.log('My number: ' + parseInt(args[2]));
 } else {
   console.log('Not a number');
 }
