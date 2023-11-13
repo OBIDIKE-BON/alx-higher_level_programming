cff17119432a53413de98630e29de4bdc418f87c
OBIDIKE-BON <bonifaceobidike@gmail.com>
2023-11-13 20:01:22 +0100

    Arguments found

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,6 +1,9 @@
 #!/usr/bin/node
 const args = process.argv;
-if (args.length > 2) {
+const size = args.length;
+if (size>3) {
+  console.log('Arguments found');
+} else if (size > 2){
   console.log('Argument found');
 } else {
   console.log('No argument');
