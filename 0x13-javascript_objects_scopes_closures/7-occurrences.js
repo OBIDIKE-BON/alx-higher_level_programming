302b4ca0f1db82d5a9f624e8507b64669bfd0c02
OBIDIKE-BON <bonifaceobidike@gmail.com>
2023-11-15 05:53:07 +0100

    initial commit

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+/*
+ * Function that returns the number of occurences in a list.
+*/
+
+exports.nbOccurences = function (list, searchElement) {
+  let count = 0;
+  list.forEach((val) => {
+    if (val === searchElement) {
+      count++;
+    }
+  });
+  return count;
+};
