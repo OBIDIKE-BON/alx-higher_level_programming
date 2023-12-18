9ccf8cf67f0d72dea593be12759bbe11098de9dd
OBIDIKE-BON <bonifaceobidike@gmail.com>
2023-12-18 14:33:00 +0100

    style

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -8,7 +8,7 @@ import MySQLdb
 if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     csr = db.cursor()
-    csr.execute("SELECT * FROM `states`"+
-    " WHERE name COLLATE utf8mb4_bin LIKE %s", ['N%'])
+    csr.execute("SELECT * FROM `states`" +
+                " WHERE name COLLATE utf8mb4_bin LIKE %s", ['N%'])
     for state in csr.fetchall():
         print(state)
