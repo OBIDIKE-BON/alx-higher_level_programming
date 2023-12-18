d9ff8b6cc51bd71762ec722ef58087e2a81643a2
OBIDIKE-BON <bonifaceobidike@gmail.com>
2023-12-18 14:02:22 +0100

    a script that lists all states with a name starting with N

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+a script that lists all states with a name starting with N
+"""
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    csr = db.cursor()
+    csr.execute("SELECT * FROM `states` WHERE `name` LIKE 'N%'")
+    for state in csr.fetchall():
+        print(state)
