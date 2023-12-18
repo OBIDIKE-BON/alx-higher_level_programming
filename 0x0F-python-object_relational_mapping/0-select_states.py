d7f74d5a2398f6f68834a6cf021bc26deb055025
OBIDIKE-BON <bonifaceobidike@gmail.com>
2023-12-18 13:18:26 +0100

    a script that lists all states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,7 +5,6 @@ import MySQLdb
 
 if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    cursor = db.Cursor()
-    cursor.execute("SELECT * FROM `states`")
-    for state in cursor.fetchall():
-        print(state)
+    csr = db.cursor()
+    csr.execute("SELECT * FROM `states`")
+    [print(state) for state in csr.fetchall()]
