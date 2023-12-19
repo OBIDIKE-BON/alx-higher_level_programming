2dd331e347cdf2d6a24f71401a2d44de945a61ba
OBIDIKE-BON <bonifaceobidike@gmail.com>
2023-12-19 11:58:14 +0100

    ok

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -14,5 +14,8 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    for state in session.query(State).order_by(State.id).first():
-        print(f"{state.id}: {state.name}")
+    state = session.query(State).order_by(State.id).first()
+    if state is None:
+        print("Nothing")
+    else:
+        print("{}: {}".format(state.id, state.name))
