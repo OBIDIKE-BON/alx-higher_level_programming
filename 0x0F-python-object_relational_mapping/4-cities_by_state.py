a7a2addbfd6813cce4093ac009956d40fce68cd0
OBIDIKE-BON <bonifaceobidike@gmail.com>
2023-12-19 02:36:50 +0100

    Lists all cities from the database with join.

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Lists all cities from the database.
+Lists all cities from the database with join.
 """
 import sys
 import MySQLdb
@@ -8,6 +8,10 @@ import MySQLdb
 if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     csr = db.cursor()
-    csr.execute("SELECT * FROM `cities`")
+    csr.execute("SELECT `cities`.`id`, `cities`.`name`, `states`.`name` \
+                 FROM `cities` \
+                INNER JOIN `states` \
+                   ON `cities`.`state_id` = `states`.`id` \
+                ORDER BY `cities`.`id`")
     for city in csr.fetchall():
         print(city)
