6aa51a7853b9efa5e7a12531a4f8cbe11fa06a1c
OBIDIKE-BON <bonifaceobidike@gmail.com>
2023-12-19 02:12:57 +0100

    style error

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -10,6 +10,6 @@ if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     csr = db.cursor()
     csr.execute("SELECT * FROM states WHERE \
-    name COLLATE utf8mb4_bin = %s ORDER BY id ASC",[sys.argv[4]])
+    name COLLATE utf8mb4_bin = %s ORDER BY id ASC", [sys.argv[4]])
     for state in csr.fetchall():
         print(state)
