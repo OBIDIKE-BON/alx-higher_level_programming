a01fdf819044a3c3e82c9f1862896bcce4a5e6a8
OBIDIKE-BON <bonifaceobidike@gmail.com>
2023-12-21 15:51:52 +0100

    exe

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -6,8 +6,8 @@ containing the letter a from the database.
 import sys
 from sqlalchemy import create_engine, func, collate
 from sqlalchemy.orm import sessionmaker
-from relationship_city import City
-from relationship_state import Base, State
+from relationship_city import Base, City
+from relationship_state import State
 
 if __name__ == "__main__":
     con = "mysql+mysqldb://{}:{}@localhost/{}\
