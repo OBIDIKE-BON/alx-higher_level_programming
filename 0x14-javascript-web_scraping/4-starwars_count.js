3a6fdcb4a5d5039261a3f50859c637013f952c7a
OBIDIKE-BON <bonifaceobidike@gmail.com>
2024-01-05 22:18:10 +0100

    ok

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,12 +1,14 @@
 #!/usr/bin/node
 const request = require('request');
-request(process.argv[2], function (error, body) {
+request(process.argv[2], function (error, response, body) {
   if (!error) {
     const results = JSON.parse(body).results;
-    console.log(results.reduce((count, movie) => {
-      return movie.characters.find((character) => character.endsWith('/18/'))
-        ? count + 1
-        : count;
-    }, 0));
+    let count = 0;
+    results.forEach(function (movie) {
+      if (movie.characters.find((character) => character.endsWith('/18/'))) {
+        count++;
+      }
+    });
+    console.log(count);
   }
 });
