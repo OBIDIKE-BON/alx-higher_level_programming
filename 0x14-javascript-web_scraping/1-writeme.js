7f356763ff97833744eced066c4bc67d30a8b3a2
OBIDIKE-BON <bonifaceobidike@gmail.com>
2024-01-05 06:06:42 +0100

    ok

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -2,7 +2,5 @@
 const fs = require("fs")
 console.log(process.argv[0], "\n\r", process.argv[1])
 fs.writeFileSync(process.argv[2], process.argv[3], error=>{
-    if (error) {
-        console.log(error)
-    }
+    if (error) console.log(error)
 });
