8a8a20e8b1c223f17aa6ab5995471c6486d0d9ce
OBIDIKE-BON <bonifaceobidike@gmail.com>
2024-01-06 06:19:33 +0100

    ok

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,6 +1,8 @@
 #!/usr/bin/node
 const fs = require('fs');
 const request = require('request');
-request(process.argv[2], function(error, response, body){
-    fs.writeFileSync(process.argv[3], body)
+request(process.argv[2], function (error, response, body) {
+  if (!error) {
+    fs.writeFileSync(process.argv[3], body);
+  }
 });
